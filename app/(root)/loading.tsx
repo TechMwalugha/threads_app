8114cd58b8a9ea337055478619386d6bb3da57ff
TechMwalugha <emmanuelmwalugha001@gmail.com>
2023-10-12 22:24:38 +0300

    fourth commit

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+    return <div className="text-light-2">Loading...</div>
+}
+
---
+++ lib/mongoose.ts
@@ -0,0 +1,21 @@
+import { CLERK_JS_URL } from '@clerk/nextjs/server';
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+export const connectToDB = async () => {
+     mongoose.set('strictQuery', true)
+
+     if(!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+     if(isConnected) return console.log("Already connected to MongoDB")
+
+     try {
+        await mongoose.connect(process.env.MONGODB_URL)
+
+        isConnected = true
+
+        console.log("Connect to MongoDB")
+     } catch (error: any) {
+        console.log(error.message)
+     }
+}
