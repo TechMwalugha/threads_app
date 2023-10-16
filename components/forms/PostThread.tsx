0458606d1c8987d45c0b7460dd2189224feff0e9
TechMwalugha <emmanuelmwalugha001@gmail.com>
2023-10-16 17:39:15 +0300

    creating threads update

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -17,6 +17,7 @@ import { usePathname, useRouter } from 'next/navigation'
 
 // import { updateUser } from '@/lib/actions/user.action'
 import { ThreadValidation } from '@/lib/validations/thread'
+import { createThread } from "@/lib/actions/thread.actions"
 
 interface Props {
  user: {
@@ -46,7 +47,49 @@ function PostThread({userId} : {userId: string}) {
         }
       })
 
-    return <h1>Post Thread Form</h1>
+      const onSubmit = async (values: z.infer<typeof ThreadValidation>) =>{
+
+        await createThread({ 
+          text: values.thread,
+          author: userId,
+          communityId: null,
+          path: pathname
+           })
+
+           router.push("/")
+      }
+
+    return (
+      <Form {...form}>
+      <form 
+      onSubmit={form.handleSubmit(onSubmit)} 
+      className="mt-10 flex flex-col justify-start gap-10">
+      
+
+      <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className='flex flex-col gap-3 w-full'>
+              <FormLabel className='text-base-semibold text-light-2'>Content</FormLabel>
+          
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea
+                rows={15}
+                {...field}
+                />
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+       </form>
+      </Form>
+    )
 }
 
 export default PostThread
